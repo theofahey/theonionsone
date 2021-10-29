02d193364e70da1e2c33cc1679b6e3e7d3bdeb5c
tomas-20 <tacuna20@stuy.edu>
2021-10-29 19:07:33 -0400

    added the add_values function

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -5,3 +5,6 @@ def value_exists(db, table_name, field_name, value):
     return bool(c.fetchone())
 def list_to_string(lst):
     return "(" + ", ".join(lst) + ")"
+def add_values(db, table_name, values):
+    value_string = list_to_string(values)
+    c.execute("INSERT INTO " + table_name + " VALUES " + value_string)
