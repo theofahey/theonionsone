246f87a0b23765026510cd15fa4d261205233564
tomas-20 <tacuna20@stuy.edu>
2021-10-29 19:09:39 -0400

    added spacing between functions

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -1,10 +1,13 @@
 import sqlite3
+
 def value_exists(db, table_name, field_name, value):
     c = db.cursor()
     c.execute("SELECT 1 FROM " + table_name + " WHERE " + field_name + " = '" + value + "'")
     return bool(c.fetchone())
+
 def list_to_string(lst):
     return "(" + ", ".join(lst) + ")"
+
 def add_values(db, table_name, values):
     value_string = list_to_string(values)
     c.execute("INSERT INTO " + table_name + " VALUES " + value_string)
