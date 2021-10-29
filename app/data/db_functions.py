65ed4d5fc6a53b1e87f52d837358af13c71a1630
tomas-20 <tacuna20@stuy.edu>
2021-10-29 19:03:33 -0400

    added the list_to_string function

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -3,3 +3,5 @@ def value_exists(db, table_name, field_name, value):
     c = db.cursor()
     c.execute("SELECT 1 FROM " + table_name + " WHERE " + field_name + " = '" + value + "'")
     return bool(c.fetchone())
+def list_to_string(lst):
+    return "(" + ", ".join(lst) + ")"
