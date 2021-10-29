365729f97ba39479f4362a447c4edf72e168ec72
turtlelazy <ishraqmahid@gmail.com>
2021-10-29 08:27:08 -0400

    init app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,7 @@
-from flask import app
+from flask import Flask  # facilitate flask webserving
+from flask import render_template  # facilitate jinja templating
+
+debug = True
+@app.route("/", methods=['GET', 'POST'])
+def disp_homepage():
+    return ""
