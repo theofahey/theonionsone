c0d0e86261d211e1b46008f198c82bd0c1347f19
tomas-20 <tacuna20@stuy.edu>
2021-10-29 18:55:06 -0400

    added the value_exists function

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -1,1 +1,5 @@
 import sqlite3
+def value_exists(db, table_name, field_name, value):
+    c = db.cursor()
+    c.execute("SELECT 1 FROM " + table_name + " WHERE " + field_name + " = '" + value + "'")
+    return bool(c.fetchone())
