0438a16ba38b62c9f1b1ce9958859369ce0fb7d8
tomas-20 <tacuna20@stuy.edu>
2021-10-29 19:30:06 -0400

    added the create_table function

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -1,12 +1,21 @@
 import sqlite3
 
+def list_to_string(lst):
+    return "(" + ", ".join(lst) + ")"
+
+def add_type(field_name):
+    return "TEXT " + field_name
+
+def create_table(db, table_name, field_names):
+    c = db.cursor()
+    fields = map(add_type, field_names)
+    field_string = list_to_string(fields)
+    c.execute("CREATE TABLE IF NOT EXISTS " + table_name + " " + field)
+
 def value_exists(db, table_name, field_name, value):
     c = db.cursor()
     c.execute("SELECT 1 FROM " + table_name + " WHERE " + field_name + " = '" + value + "'")
     return bool(c.fetchone())
-
-def list_to_string(lst):
-    return "(" + ", ".join(lst) + ")"
 
 def add_values(db, table_name, values):
     value_string = list_to_string(values)
