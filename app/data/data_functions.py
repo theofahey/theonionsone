a40b6f9a2b920db1a1a1f80fa9b4a0b8a4648861
tomas-20 <tacuna20@stuy.edu>
2021-10-30 18:37:30 -0400

    added user_exists, add_user, print_users, and clear_users to data_functions

--- app/data/data_functions.py
+++ app/data/data_functions.py
@@ -1,2 +1,18 @@
 from db_functions import *
 from sqlite3 import connect
+
+def user_exists(username):
+    data = connect("data.db")
+    return value_exists(data, "users", "username", username)
+
+def add_user(username, password):
+    data = connect("data.db")
+    add_values(data, "users", [username, password])
+
+def print_users():
+    data = connect("data.db")
+    print_table(data, "users")
+
+def clear_users():
+    data = connect("data.db")
+    clear_table(data, "users")
