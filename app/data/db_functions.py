05155fa0baf721904acac747ef370663120c8e6e
tomas-20 <tacuna20@stuy.edu>
2021-11-01 15:21:11 -0400

    changed the position of create_table and add_values in db_functions.py

--- app/data/db_functions.py
+++ app/data/db_functions.py
@@ -14,6 +14,18 @@ def create_table(db, table_name, field_names):
     c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} {field_string}")
     db.commit()
 
+def add_values(db, table_name, values):
+    c = db.cursor()
+    quoted_values = map(quote, values)
+    value_string = list_to_string(quoted_values)
+    c.execute(f"INSERT INTO {table_name} VALUES {value_string}")
+    db.commit()
+
+def set_value(db, table_name, field_name, value, target_field, new_value):
+    c = db.cursor()
+    c.execute(f"UPDATE {table_name} SET {target_field} = '{new_value}' WHERE {field_name} = '{value}'")
+    db.commit()
+
 def get_value_helper(db, table_name, field_name, value, target_field):
     c = db.cursor()
     c.execute(f"SELECT {target_field} FROM {table_name} WHERE {field_name} = '{value}'")
@@ -24,18 +36,6 @@ def get_value(db, table_name, field_name, value, target_field):
 
 def value_exists(db, table_name, field_name, value):
     return bool(get_value_helper(db, table_name, field_name, value, "1"))
-
-def set_value(db, table_name, field_name, value, target_field, new_value):
-    c = db.cursor()
-    c.execute(f"UPDATE {table_name} SET {target_field} = '{new_value}' WHERE {field_name} = '{value}'")
-    db.commit()
-
-def add_values(db, table_name, values):
-    c = db.cursor()
-    quoted_values = map(quote, values)
-    value_string = list_to_string(quoted_values)
-    c.execute(f"INSERT INTO {table_name} VALUES {value_string}")
-    db.commit()
 
 #just for testing:
 def clear_table(db, table_name):
