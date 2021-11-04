2ac3f3012917ce66518eaa29aea8f610c1fdea9b
turtlelazy <ishraqmahid@gmail.com>
2021-11-03 21:21:51 -0400

    removed testfolder0 due to weirdness

--- testfolder0/app/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask  # facilitate flask webserving
-from flask import render_template  # facilitate jinja templating
-
-debug = True
-@app.route("/", methods=['GET', 'POST'])
-def disp_homepage():
-    return ""
