c591a71a5cf1843b7f5607834209deb4832250b2
turtlelazy <ishraqmahid@gmail.com>
2021-11-03 22:50:33 -0400

    register turned into POST req; registering boolean variables created

--- app/app.py
+++ app/app.py
@@ -17,40 +17,52 @@ app.secret_key = urandom(24)
 
 @app.route("/", methods=['GET', 'POST'])
 def welcome():
+    '''
+    Welcome Page
+    '''
     if 'username' in session.keys() and session['username']:
         return render_template("home_Page.html", user = session['username'])
     else:
         return render_template('login_Page.html')
 
-#Login page
-
-@app.route("/welcome", methods=['GET', 'POST'])
-def display_login():
-    return render_template('login_Page.html')
-
-#Register page
-
 
 @app.route("/register", methods=['GET', 'POST'])
 def disp_registerpage():
+    '''
+    register page
+    '''
     return render_template('register.html')
+
+
 
 #Checks the register to make sure everything is good
 #Password and confirm password should be the same
-
-
 @app.route("/check_register", methods=['GET', 'POST'])
 def check_register():
-    user = request.args.get('username')
-    passwd = request.args.get('password')
-    conpass = request.args.get('confirm_password')
+    '''
+
+    '''
+    username = request.form.get('username')
+    password = request.form.get('password')
+    con_password = request.form.get('confirm_password')
 
     #grabs stuff
-    if passwd == conpass and bool(user) and bool(passwd) and bool():
+    password_requirements = password == con_password and bool(password)
+
+    username_conflict = False or (not bool(username))
+
+    if password_requirements and (not username_conflict):
         return render_template('login_Page.html', extra_Message="Successfully Registered")
+
     else:
         #Need to create different error messages for different things
-        return render_template('register.html', extra_Message="Something went wrong")
+        extra_Message = "An error has been made trying to register you."
+        if not password_requirements:
+            extra_Message = "Password requirements not met. Check to see that password is at least one character and that password confirmation matches"
+        
+        elif username_conflict:
+            extra_Message = "Username may already be in use, or does not contain at least one character"
+        return render_template('register.html', extra_Message=extra_Message)
 
 
 @app.route("/logout", methods=['GET', 'POST'])
@@ -60,9 +72,15 @@ def logout():
 
 @app.route("/auth_ed", methods=['POST'])
 def authenticate():
+    '''
+    authorization page; redirects and logs in if credentials work, loads login template if not
+    '''
+
+    #retrieve from FORM instead of ARGS because we are retrieving from POST method
     username = request.form.get('username')
     password = request.form.get('password')
     
+    #authflow variable
     loginAuthorized = False
 
     if loginAuthorized:
