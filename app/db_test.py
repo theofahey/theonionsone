4cf6e18b20226db050f6583157d5af36c086f643
tomas-20 <tacuna20@stuy.edu>
2021-11-04 20:28:23 -0400

    moved test.py out of the data folder and renamed it to db_test.py

--- app/db_test.py
+++ app/db_test.py
@@ -1,4 +1,4 @@
-from data_functions import *
+from data.data_functions import *
 
 def username_test(username):
     if user_exists(username):
@@ -21,6 +21,10 @@ def password_test(username, password):
         no = " NOT"
     print(f"{password} is{no} the correct password for {username}")
 
+def show_edited_stories(username):
+    edited_stories = list(get_edited_stories(username))
+    print(f"{username} has edited: {edited_stories}")
+
 initialize()
 clear_all()
 
@@ -29,18 +33,18 @@ add_user("joe", "okaypassword")
 username_test("bob")
 username_test("joe")
 username_test("carlos")
+add_user("carlos", "coolpassword")
 password_test("bob", "goodpassword")
 password_test("joe", "okaypassword")
 password_test("bob", "idk")
 
 add_story("harry potter")
-add_new_part("harry potter", "there once was a boy named harry")
-add_new_part("harry potter", "he did stuff at school")
-add_new_part("harry potter", "he saved the world or something yay")
+add_new_part("harry potter", "there once was a boy named harry", "bob")
+add_new_part("harry potter", "he did stuff at school", "carlos")
 add_story("lord of the rings")
-add_new_part("lord of the rings", "frodo baggins liked submarines")
-add_new_part("lord of the rings", "he sailed through the oceans")
-add_new_part("lord of the rings", "he sailed through all the streams")
+add_new_part("lord of the rings", "frodo baggins liked submarines", "joe")
+add_new_part("lord of the rings", "he sailed through the oceans", "carlos")
+add_new_part("lord of the rings", "he sailed through all the streams", "bob")
 print("\nmost recent add to harry potter:")
 print(get_new_part("harry potter"))
 print("\nentire text of harry potter:")
@@ -51,3 +55,6 @@ story_test("lord of the rings")
 story_test("cheese")
 print(list(get_usernames()))
 print(list(get_titles()))
+show_edited_stories("bob")
+show_edited_stories("joe")
+show_edited_stories("carlos")
